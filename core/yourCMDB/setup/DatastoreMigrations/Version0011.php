2eade0bedf61dda4a3961be1046e2b97d88fd5d9
michael-batz <michael@yourcmdb.org>
2015-09-27 11:07:51 +0200

    added update class for version 0.11 datastore migration

---
+++ core/yourCMDB/setup/DatastoreMigrations/Version0011.php
@@ -0,0 +1,49 @@
+<?php
+/********************************************************************
+* This file is part of yourCMDB.
+*
+* Copyright 2013-2015 Michael Batz
+*
+*
+* yourCMDB is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* yourCMDB is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with yourCMDB.  If not, see <http://www.gnu.org/licenses/>.
+*
+*********************************************************************/
+namespace yourCMDB\setup\DatastoreMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+
+/**
+* DataStore migration for yourCMDB 0.11
+* naming convention for classes: VerionXXYY
+* 	where 	XX -> major version number
+*		YY -> minor version number
+* @author Michael Batz <michael@yourcmdb.org>
+*/
+class Version0011 extends AbstractMigration
+{
+	public function up(Schema $schema)
+	{
+		//nothing to do, since there are no database changes
+		;
+	}
+
+	public function down(Schema $schema)
+	{
+		//downgrade not possible
+		;
+	}
+}
+?>
